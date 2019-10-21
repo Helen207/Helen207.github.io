544dbea18d0ea34eab23000f00723049d9876ed3
Helena Hajdukova <hajdukovah@gmail.com>
2019-10-21 20:40:42 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	// Transition effect for navbar
+	$(window).scroll(function() {
+		// checks if window is scrolled more than 500px, adds/removes solid class
+		if ($(this).scrollTop() > 262) {
+			$('.navbar').addClass('solid'),
+				$('.navbar-brand').addClass('solid'),
+				$('.navbar-dark .navbar-nav .nav-link').addClass('solid');
+		} else {
+			$('.navbar').removeClass('solid'),
+				$('.navbar-brand').removeClass('solid'),
+				$('.navbar-dark .navbar-nav .nav-link').removeClass('solid');
+		}
+	});
+});
+
+//function that calculates the number of months I have been learning full stack dev since ... till now.
+function getMonths() {
+	const dateNow = new Date();
+	const dateStart = new Date('06/01/2019'); //mm/dd/yyyy
+	diffMonths = Math.abs(dateStart.getMonth() - dateNow.getMonth());
+	return diffMonths;
+}
+document.getElementById('date').innerHTML = getMonths();
